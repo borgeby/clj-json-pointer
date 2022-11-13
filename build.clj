75fc1f0ae2123a5caa32dd6d8fc21f88c4ee9547
Anders Eknert <anders@eknert.com>
2022-11-13 23:52:35 +0100

    Prepare for publishing
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- build.clj
+++ build.clj
@@ -2,19 +2,29 @@
   (:refer-clojure :exclude [compile])
   (:require [clojure.tools.build.api :as b]))
 
+(def version "0.1.0")
+
+(def lib 'borge.by/clj-json-pointer)
 (def basis (b/create-basis {:project "deps.edn"}))
 (def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn clean [_]
   (b/delete {:path "target"}))
 
 (defn compile [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src" "test"]
-               :target-dir class-dir})
-  (b/compile-clj {:basis basis
-                  :src-dirs ["src" "test"]
-                  :class-dir class-dir}))
+  (b/copy-dir    {:src-dirs ["src"] :target-dir class-dir})
+  (b/compile-clj {:src-dirs ["src"] :basis basis :class-dir class-dir}))
 
-; :build     {:deps       {org.clojure/tools.build {:mvn/version "0.8.4"}}
-;              :ns-default build}
+(defn pom [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]}))
+
+(defn jar [_]
+  (pom nil)
+  (b/copy-dir {:src-dirs ["src"] :target-dir class-dir})
+  (b/jar {:class-dir class-dir :jar-file jar-file}))
